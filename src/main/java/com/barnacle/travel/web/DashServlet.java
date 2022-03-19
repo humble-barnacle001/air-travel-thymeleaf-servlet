c8d72e095225423f0ad6380843779a56f8dc31e8
Barnacle Nightshade <58736240+humble-barnacle001@users.noreply.github.com>
2022-03-19 06:01:58 +0000

    fix: refactor code and missing template

--- src/main/java/com/barnacle/travel/web/DashServlet.java
+++ src/main/java/com/barnacle/travel/web/DashServlet.java
@@ -1,9 +1,9 @@
 package com.barnacle.travel.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Optional;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.barnacle.travel.config.CustomWebContext;
+import com.barnacle.travel.config.TemplateEngineUtil;
 import com.barnacle.travel.database.models.User;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 @WebServlet("/dashboard")
 public class DashServlet extends HttpServlet {
@@ -19,20 +24,24 @@ public class DashServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
 
-        Optional<Boolean> isLoggedIn = Optional.ofNullable((Boolean) session.getAttribute("isLoggedIn"));
+        Optional<Boolean> isLoggedIn = Optional.ofNullable(
+                (Boolean) session.getAttribute("isLoggedIn"));
         if (!isLoggedIn.orElse(false))
             resp.sendRedirect(req.getContextPath() + "/auth");
         else {
-            PrintWriter pw = resp.getWriter();
-            resp.setContentType("text/html");
-            pw.print((((User) session.getAttribute("user")).getIsManager()
-                    ? "Admin"
-                    : "User") + " ");
-            pw.println("DASHBOARD");
-
-            pw.println("<form action='/logOut' method='get'>");
-            pw.println("<input type='submit' value='Log Out' />");
-            pw.println("</form>");
+            Optional<User> user = Optional.ofNullable((User) session.getAttribute("user"));
+            if (user.isPresent()) {
+                if (user.get().getIsManager()) {
+                    ServletContext sc = req.getServletContext();
+                    TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(sc);
+                    WebContext context = CustomWebContext.generateContext(req, resp, sc);
+                    resp.setContentType("text/html;charset=UTF-8");
+                    engine.process("dashboard", context, resp.getWriter());
+                } else
+                    resp.sendRedirect(req.getContextPath() + "/search");
+            } else {
+                resp.sendRedirect(req.getContextPath() + "/auth");
+            }
         }
     }
 }
